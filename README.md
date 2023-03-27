61a711a52faefc7524a7b67cdf0f95fb4d9acee9
Prateek Reddy <prateekreddyy@gmail.com>
2023-03-27 12:16:08 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Cluster Selection Bot
+
+A script to select clusters on Marlin network at every epoch.
+
+## Installation
+
+Nodejs can be installed using instructions [here](https://nodejs.org/en/download/package-manager) and then install dependencies using  `npm i`.
+
+## How to run ?
+
+Create a `.env` file based on `.env.example`. Fill the following fields
+
+```
+# url for Arbitrum One RPC
+ARB_RPC_URL=
+# Private key of the address used to send txs for cluster selection
+SIGNING_KEY=
+# Address of the cluster selector contract
+CLUSTER_SELECTOR_ADDRESS=
+```
+
+`CLUSTER_SELECTOR_ADDRESS` for Eth based network on arbitrum one mainnet is `0xf1E4c2d4B2996C7c34b5a6136861D7385Cb0E843`([link](https://arbiscan.io/address/0xf1E4c2d4B2996C7c34b5a6136861D7385Cb0E843)).
+
+Script to select clusters can be run by using `npm start`.
+
+## Reward
+
+Invoking cluster selection for an epoch is essential to ensure there is a churn in clusters selected for propagation in the network which results in selected clusters being able to propagate blocks in the network and receive tickets. Tickets when submitted by receivers will result in propagated clusters being rewarded. This ensures there is a inbuilt incentive for clusters who aren't selected in an epoch to invoke cluster selection. 
+
+To bootstrap this process a reward equal to worst case transaction fee for cluster selection is given as reward to `msg.sender` of the tx that selects clusters for an epoch in `ETH`.
+
+### Calculation of reward
+
+Transaction cost on Arbitrum has 2 components
+
+1. L2 execution and storage cost
+2. L1 calldata cost
+
+L2 execution and storage cost can be calculated by gas used to execute `selectClusters` method. Based on simulations a worst case gas cost (which won't be reached in most cases) will be used to cover the L2 gas cost as well as give a small reward.
+
+L1 calldata cost is calculated based on ArbGasInfo [precompile](https://developer.arbitrum.io/arbos/precompiles). Base gas fee for L2 tx and the calldata (only method sig which is 4 bytes) are considered as part of this cost.
+
+Sum of the above costs are considered towards calculation of rewards.
---
+++ index.js
@@ -0,0 +1,49 @@
+const { ethers } = require("ethers");
+require('dotenv').config();
+
+const provider = new ethers.JsonRpcProvider(process.env.ARB_RPC_URL);
+const signer = new ethers.Wallet(process.env.SIGNING_KEY, provider);
+const ABI = new ethers.Interface([
+    "function selectClusters()", 
+    "function EPOCH_LENGTH() view returns(uint256)", 
+    "function getCurrentEpoch() view returns(uint256)",
+    "function getClusters(uint256 epochNumber) view returns(address[])"
+]);
+const contractAddress = process.env.CLUSTER_SELECTOR_ADDRESS;
+const contract = new ethers.Contract(contractAddress, ABI, provider);
+
+async function handleSelection() {
+    const epochLength = parseInt(await contract.EPOCH_LENGTH());
+
+    await selectClusters();
+    setInterval(async () => {
+        await selectClusters();
+    }, epochLength*1000);
+}
+
+async function selectClusters() {
+    try {
+        const epoch = Number(await contract.getCurrentEpoch());
+        console.log(`${(new Date()).toJSON()} Current Epoch is  ${epoch}`);
+        try {
+            await contract.getClusters(epoch+1);
+            console.log(`${new Date().toJSON()} Clusters already selected for epoch ${epoch+1}`);
+            console.log(`${new Date().toJSON()} Waiting to select clusters for epoch ${epoch+2}`);
+            return;
+        } catch(e) { console.log(`${new Date().toJSON()} Clusters not yet selected for epoch ${epoch+1}`); }
+
+        console.log(`${new Date().toJSON()} Preparing tx to select clusters for epoch ${epoch+1}`);
+        const tx = await contract.connect(signer).selectClusters();
+        console.log(`${new Date().toJSON()} Tx submitted to select clusters for epoch ${epoch+1}. Hash: ${tx.hash}`);
+        const receipt = await tx.wait();
+        if(receipt.status == 1) {
+            console.log(`${new Date().toJSON()} SUCCESS: Clusters selected for epoch ${epoch+1}. Hash: ${tx.hash}`);
+        } else {
+            console.log(`${new Date().toJSON()} FAILED: Missed clusters selection for epoch ${epoch+1}. Hash: ${tx.hash}`);
+        }
+    } catch(error) {
+        console.log(`${new Date().toJSON()} FAILED: Cluster Selection for Epoch didn't go through ${error}`);
+    }
+}
+
+handleSelection();
